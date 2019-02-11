f4e8ecd64c8b5f1a04a7223df8e04199918fed7e
rdaley321 <rdaley321@yahoo.com>
2019-02-11 15:55:16 -0600

    removed comma

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
     username: {type: String, required: true, index: { unique: true } },
-    password: {type: String, required: true},
+    password: {type: String, required: true}
 });
 
 
